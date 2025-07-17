c2d4d3b290985a474a63b1e3a138a588644bea7e
fmazzarotto <37803729+mazzatramp@users.noreply.github.com>
2025-07-17 17:46:31 +0200

    Add marix and rank prefix, add MPI Barriers

--- mm_par_ring.c
+++ mm_par_ring.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
+#include <time.h>
 
 #define MATRIX_RAND_MAX 10
 
@@ -32,24 +33,26 @@ int print_arr(int* a, int len) {
     printf("\n");
 }
 
-void chain_array_print(int rank, int *a, int len) {
-    MPI_Status *status;
+void chain_array_print(int rank, int *a, int len, char prefix) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Status status;
 
     const int next = (rank+1)%len;
     const int prev = (rank-1)<0 ? len-1 : rank-1;
 
     if (rank == 0) {
-        printf("%d: ", rank);
+        printf("%c%d: ", prefix, rank);
         print_arr(a, len);
         MPI_Send(NULL, 0, MPI_INT, next, 0, MPI_COMM_WORLD);
     } else {
-        MPI_Recv(NULL, 0, MPI_INT, prev, 0, MPI_COMM_WORLD, status);
-        printf("%d: ", rank);
+        MPI_Recv(NULL, 0, MPI_INT, prev, 0, MPI_COMM_WORLD, &status);
+        printf("%c%d: ", prefix, rank);
         print_arr(a, len);
         if (rank != len -1) {
             MPI_Send(NULL, 0, MPI_INT, next, 0, MPI_COMM_WORLD);
         }
     }
+    MPI_Barrier(MPI_COMM_WORLD);
 }
 
 int main(int argc, char **argv) {
@@ -57,12 +60,15 @@ int main(int argc, char **argv) {
     int my_rank, n_procs;
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
-    MPI_Status *status;
+    MPI_Status status;
+
+    //random seed different for each process
+    srand(time(NULL) + my_rank * 2435); //unimportant magic number
 
     const int next = (my_rank+1) % n_procs;
     const int prev = (my_rank-1)<0 ? n_procs-1 : my_rank-1;
 
-    // First assumpion: n_procs = rows = cols
+    // The assumpion is that n_procs = rows = cols
 
     int *columnB = (int*)malloc(n_procs * sizeof(int));
     int *rowA = (int*)malloc(n_procs * sizeof(int));
@@ -71,9 +77,14 @@ int main(int argc, char **argv) {
     fill_rand(columnB, n_procs);
     fill_rand(rowA, n_procs);
 
-    chain_array_print(my_rank, rowA, n_procs);
-    chain_array_print(my_rank, columnB, n_procs);
-
+    if (my_rank == 0) {
+        printf("Matrix A:\n");
+    }
+    chain_array_print(my_rank, rowA, n_procs, 'A');
+    if (my_rank == 0) {
+        printf("Matrix B:\n");
+    }
+    chain_array_print(my_rank, columnB, n_procs, 'B');
     int *columnC = (int*)malloc(n_procs * sizeof(int));
 
     for (int step = 0; step < n_procs; step++) {
@@ -83,13 +94,16 @@ int main(int argc, char **argv) {
         MPI_Sendrecv(
             rowA, n_procs, MPI_INT, next, 0, //send
             buffer, n_procs, MPI_INT, prev, 0, //recv
-            MPI_COMM_WORLD, status
+            MPI_COMM_WORLD, &status
         );
 
         swap_ptrs(&rowA, &buffer);
     }
 
-    chain_array_print(my_rank, columnC, n_procs);
+    if (my_rank == 0) {
+        printf("Matrix C:\n");
+    }
+    chain_array_print(my_rank, columnC, n_procs, 'C');
 
     free(rowA);
     free(columnB);
